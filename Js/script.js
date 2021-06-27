edd1ffc4a36e97d581aecf7efc07c1f39fdd8c0f
E-Martynenko1982 <motor8103@gmail.com>
2021-06-27 22:55:23 +0300

    1й коммит

---
+++ Js/script.js
@@ -0,0 +1,14 @@
+var myNum = 10;
+var myStr = 'строка';
+var myBool = true;
+var myArr = [1, 2, 3, 4, 5];
+var myObj = {
+  first: 'First Name',
+  last: 'Last Name'
+};
+console.log('myNum: ', myNum);
+console.log('myStr:', myStr);
+console.log('myBool:', myBool);
+console.log('myArr:', myArr);
+console.log('myObj:', myObj);
+
