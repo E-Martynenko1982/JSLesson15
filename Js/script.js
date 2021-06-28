cdf8239fd2b73c99de85c1716e893b74787e4afa
E-Martynenko1982 <motor8103@gmail.com>
2021-06-28 16:20:13 +0300

    домашка 15

--- Js/script.js
+++ Js/script.js
@@ -12,3 +12,52 @@ console.log('myBool:', myBool);
 console.log('myArr:', myArr);
 console.log('myObj:', myObj);
 
+
+var decimal2 = myNum.toFixed(2);
+
+var i = 1;
+console.log('префиксный инкремент:', ++i);
+console.log('постфиксный инкремент:', i++);
+console.log('Значение i:', i);
+console.log('префиксный декремент:', --i);
+console.log('префиксный декремент:', i--);
+console.log('Значение i:', i);
+
+
+var myTest = 20;
+myTest += myNum;
+console.log(myTest);
+myTest -= myNum;
+console.log(myTest);
+myTest *= myNum;
+console.log(myTest);
+myTest /= myNum;
+console.log(myTest);
+myTest %= myNum;
+console.log(myTest);
+
+var myPi = Math.PI;
+console.log('myPi', myPi);
+var myRound = Math.round(89.279);
+console.log('myRound', myRound);
+var myRandom = Math.random() * 10;
+console.log('myRandom', myRandom);
+var myPow = Math.pow(3, 5);
+console.log('myPow', myPow);
+
+
+var strObj = {str: 'Мама мыла раму, рама мыла маму'};
+strObj.lenth = strObj.str.length;
+console.log('strObj', strObj);
+
+var isRamaPos = strObj.str.indexOf('рама');
+console.log('isRamaPos', isRamaPos);
+
+var strReplace = strObj.str.replace('мыла', 'моет');
+
+strReplace = strReplace.replace('рама', 'Рама');
+strReplace = strReplace.replace('мыла', 'держит');
+console.log('strReplace', strReplace);
+
+console.log(myStr.toUpperCase());
+console.log('Любая строка'.toLowerCase());
